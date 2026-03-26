65d64b41358bb7a6d764d36d3520e692fe7078a9
Asif Afghan <masifafghan7@gmail.com>
2026-03-26 14:54:13 +0500

    align home page

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -23,31 +23,34 @@ function home(){
           </div>
          </div>
          </nav>
-         <section id="homebg" className="mx-5 animate__animated animate__fadeInUp border border-black">
+         <section id="homebg" className="animate__animated animate__fadeInUp border border-black">
             <button type="button" class="btn">Shop now</button>
         </section>
-        <section className="d-flex mx-5 m-1">
-         <div className="w-50 ms-1 text-white"  id='rad1' >
-           <h1 classname='m-4 pe-5 pt-5'>About</h1>
-            <h4 classname='me-5 mt-5 animate__animated animate__fadeInUp'>
+        <section className="d-flex m-1">
+         <div className="w-50  p-5 border border-black text-white " >
+           <h1 classname=''>About</h1>
+            <h4 classname=' animate__animated animate__fadeInUp'>
             Refers to the feelings, opinions, or attitudes that people hold toward a
             particular topic, person, or issue.
             </h4>
          </div>
          <div className="w-50 " >
-           <img className="w-100 h-100" src="/Images/sents-5.jpg" alt="About" id='rad2' class='img12' />
+           <img className="w-100 h-100" src="/Images/sents-5.jpg" alt="About"  />
          </div>
        </section>
-       <section className="d-flex mx-5">
+       <section className="d-flex ">
          <div className="w-50 " >
-           <img className="w-100 h-100 " src="/Images/sents-5.jpg" alt="About" id='rad2' class='img12' />
+           <img className="w-100 h-100 " src="/Images/smell.png" alt="About"/>
          </div>
-         <div className="w-50 ms-1 text-white"  id='rad1' >
-           <h1 classname='m-4 pe-5 pt-5'>About</h1>
-            <h4 classname='me-5 mt-5  animate__animated animate__fadeInUp'>
-            Refers to the feelings, opinions, or attitudes that people hold toward a
-            particular topic, person, or issue.
-            </h4>
+         <div className="w-50 p-5 border border-black text-white" >
+            <br/>
+          <br/>
+           <h1 class='text-white'>Contact US</h1>
+           <br/>
+           <h4 className='  text-white'>Name</h4>
+           <input classname='w-75'  type="text"></input>
+            <h4 className='my-4 mb-2 text-white'>Email</h4>
+           <input classname='m-4 px-5 w-75 '  type="text"></input>
          </div>
        </section>
         <section className="d-flex bg-dark  me-2"  id="rad6">
