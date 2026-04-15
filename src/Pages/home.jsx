2bc0ed4c1af9454c6be1e0146b5ca1c2b0f985e0
Asif Afghan <masifafghan7@gmail.com>
2026-04-15 14:35:59 +0500

    bootsratp nav

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,28 +1,23 @@
 import { Link } from "react-router-dom";
 import '../App.css';
-import 'animate.css'
+import 'animate.css';
+
 function home(){
     return (
-        <>
-         {/* Nav Bar */}
-         <nav className="navbar navbar-expand-sm text-light bg-dark">
-         <div className="container-fluid">
-          <button className="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mb-2 mb-lg-0 m-auto">
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/Store">Store</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link text-white" to="/about">About</Link>
-              </li>
-            </ul>
-            <span className="navbar-brand m-auto text-white">Sentiments</span>
+        <><nav className="navbar navbar-expand-lg bg-body-tertiary">
+           <div className="container-fluid">
+             <a className="navbar-brand" href="/">Sentiments</a>
+             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+               <span className="navbar-toggler-icon"></span>
+             </button>
+             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+               <div className="navbar-nav">
+                 <a className="nav-link" href="/Store">Store</a>
+                 <a className="nav-link" href="/about">About</a>
+              </div>
+            </div>
           </div>
-         </div>
-         </nav>
+        </nav>
          <section id="homebg" className="animate__animated animate__fadeInUp border border-black">
             <button type="button" class="btn">Shop now</button>
         </section>
